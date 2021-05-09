74590406208e3ebd76713f67901695222c0c27d2
Josh <jsoemant@uci.edu>
2021-05-09 03:19:59 -0700

    added index and login page

---
+++ frontend/login.js
@@ -0,0 +1,56 @@
+let login_form = $("#login_form");
+
+function handleLoginResult(resultData) {
+
+    console.log("handle login response");
+    console.log(resultData);
+    console.log(resultData["status"]);
+
+    // If login succeeds, it will redirect the user to index.html
+    if (resultData["status"] === "success") {
+        if (resultData["type"] === "employee") {
+            window.location.replace("_dashboard");
+        } else {
+            window.location.replace("index.html");
+        }
+    } else {
+        // If login fails, the web page will display 
+        // error messages on <div> with id "login_error_message"
+        console.log("show error message");
+        console.log(resultData["message"]);
+        $("#login_error_message").text(resultData["message"]);
+
+        let c = $('.g-recaptcha').length;
+        for (let i=0; i<c;i++) {
+            grecaptcha.reset();
+        }
+    }
+}
+
+/**
+ * Submit the form content with POST method
+ * @param formSubmitEvent
+ */
+function submitLoginForm(formSubmitEvent) {
+    console.log("submit login form");
+    /**
+     * When users click the submit button, the browser will not direct
+     * users to the url defined in HTML form. Instead, it will call this
+     * event handler when the event is triggered.
+     */
+    formSubmitEvent.preventDefault();
+
+    $.ajax(
+        "api/login", {
+            dataType: "json",
+            method: "POST",
+            // Serialize the login form to the data sent by POST request
+            data: login_form.serialize(),
+            success: handleLoginResult
+        }
+    );
+}
+
+// Bind the submit action of the form to a handler function
+login_form.submit(submitLoginForm);
+
