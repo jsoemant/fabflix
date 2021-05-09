86186747478ea0fefdf73f56c80e49fdd392bb70
Kevin T <kevintuantran1@gmail.com>
2021-05-09 02:52:03 -0700

    updating sql files directory

---
+++ sql/update-moviedb-employees.sql
@@ -0,0 +1,9 @@
+use moviedb;
+
+CREATE TABLE IF NOT EXISTS employees(
+	email varchar(50) primary key,
+	password varchar(20) not null,
+	fullname varchar(100)
+);
+
+INSERT INTO employees VALUES('classta@email.edu', 'classta', 'TA CS122B')
